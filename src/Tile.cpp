af78830f033babc91f3bf79b0aa8864ddf6521d6
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-02-09 16:51:57 -0500

    made neighbour refrences for each tile

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -8,6 +8,8 @@ Tile::Tile()
 {
 	setWidth(Config::TILE_SIZE);
 	setHeight(Config::TILE_SIZE);
+
+	setTileCost(0.0f);
 }
 
 Tile::~Tile()
@@ -24,4 +26,26 @@ void Tile::update()
 
 void Tile::clean()
 {
+}
+
+Tile* Tile::getNeighbourTile(const NeighbourTile position)
+{
+	return m_neighbours[position];
+}
+
+void Tile::setNeighbourTile(const NeighbourTile position, Tile* tile)
+{
+	m_neighbours[position] = tile;
+}
+
+float Tile::getTileCost() const
+{
+	return m_cost;
+}
+
+void Tile::setTileCost(const float cost)
+{
+	m_cost = cost;
+
+	//TODO: Display cost in label
 }
